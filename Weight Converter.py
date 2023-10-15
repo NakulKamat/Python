43d97c92f5336c0af4b5161b19a5f6078d254ed5
NakulKamat <147970776+NakulKamat@users.noreply.github.com>
2023-10-15 13:15:36 +0530

    Delete Weight Converter.py

--- Weight Converter.py
+++
@@ -1,8 +0,0 @@
-xyz = int(input('Please Enter Weight: '))
-unit = input('(L)bs or (K)g:')
-if unit.upper == "L":
-  converted= xyz * 0.45
-  print(f"You are {converted} Kilos")
-else:
-  converted = xyz // 0.45
-  print(f"You are {converted} Pounds")
